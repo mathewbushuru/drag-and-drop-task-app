beaa98eb00c3c73da8e3e3390ca3685d355095f3
mathew bushuru <bushuruwafulamathew@gmail.com>
2022-10-30 16:53:55 -0700

    create state in app  to store initial data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import initialData from './data/initial-data'
+import React, { useState } from "react";
+import initialData from "./data/initial-data";
 
 const App = () => {
-  return (
-    <div>Hello World App</div>
-  )
-}
+  const [data, setData] = useState(initialData);
 
-export default App
+  return (
+    <>
+      <div>Hello World App </div>
+      {data.columnOrder.map((columnId) => {
+        const column = data.columns[columnId];
+        const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
+        return column.title
+      })}
+    </>
+  );
+};
+
+export default App;
