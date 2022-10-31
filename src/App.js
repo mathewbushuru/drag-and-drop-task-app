2032f18b4d7ad68abd6aa764eaace69337a2e7bb
mathew bushuru <bushuruwafulamathew@gmail.com>
2022-10-31 01:09:33 -0700

    persist list reordering using onDragEnd callback, on UI without server  confirmation

--- src/App.js
+++ src/App.js
@@ -6,9 +6,54 @@ import { DragDropContext } from "react-beautiful-dnd";
 const App = () => {
   const [data, setData] = useState(initialData);
 
-  const dragEndHandler = () => {
-    console.log("Drag ended");
-    //TODO: reorder column
+  const dragEndHandler = (result) => {
+    //reorder column
+    //result.destination null if draggable not dropped in a droppable
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+    //if destination is  same as source, and order didnt change
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+    console.log(`
+        Drag ended. 
+        id:${result.draggableId}. 
+        source-id:${result.source.droppableId}.
+        source-index:${result.source.index}.
+        destination-id:${result.destination.droppableId}.
+        destination-index:${result.destination.index}.
+    `);
+
+    //Retrieve the column
+   const column = data.columns[source.droppableId];
+
+    //new taskIds array with same content as last array
+    //create new objects now instead  of mutating  state
+    const newTaskIds = Array.from(column.taskIds);
+
+    //move the task  id  from  the old index to the new index by reordering the array
+    //at source.index,  remove one item
+    newTaskIds.splice(source.index, 1);
+    //at destination.index, remove nothing but insert 1 item
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+    const newData = {
+      ...data,
+      columns: {
+        ...data.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+    setData(newData);
   };
 
   return (
